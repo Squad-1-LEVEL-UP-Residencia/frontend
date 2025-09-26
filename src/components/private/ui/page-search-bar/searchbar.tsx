b40f57a4c550a7acc61b908da33da54ae507f283
Danillo Ferreira <nilloferreiira@gmail.com>
2025-09-26 18:55:42 -0300

    hotfix/deploy

--- src/components/private/ui/page-search-bar/searchbar.tsx
+++ src/components/private/ui/page-search-bar/searchbar.tsx
@@ -4,13 +4,15 @@ import { Input } from "../input"
 import { Container } from "../container"
 
 // TODO implementar strategy
-interface SearchBarProps {}
+interface SearchBarProps {
+	search?: string
+}
 
-export function SearchBar({}: SearchBarProps) {
+export function SearchBar({ search }: SearchBarProps) {
 	return (
 		// <div className="bg-white p-6 rounded-lg flex gap-2">
 		<Container className="flex gap-2" variant="card">
-			<Input placeholder="Pesquisar usuários" />
+			<Input placeholder="Pesquisar usuários" value={search} />
 			<Button className="px-3">
 				<FilterIcon size={16} />
 			</Button>
