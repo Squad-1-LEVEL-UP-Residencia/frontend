6559da119f50e99a52f35c8809203f952ae6f391
Danillo Ferreira <nilloferreiira@gmail.com>
2025-10-01 01:04:45 -0300

    feat: sidebar collapsed by default

--- src/components/private/base-layout.tsx
+++ src/components/private/base-layout.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from "@tanstack/react-query"
 import { queryClient } from "@/data/react-query"
 
 export function BaseLayout({ children }: { children: React.ReactNode }) {
-	const [collapsed, setCollapsed] = useState(false)
+	const [collapsed, setCollapsed] = useState(true)
 
 	const toggleCollapsed = () => {
 		setCollapsed(!collapsed)
@@ -19,9 +19,7 @@ export function BaseLayout({ children }: { children: React.ReactNode }) {
 			<div className="flex flex-col w-full min-h-screen bg-white">
 				<Header collapsed={collapsed} toggleCollapsed={toggleCollapsed} />
 				<QueryClientProvider client={queryClient}>
-					<main className="px-8 pt-8 w-full text-text-primary h-full rounded-xl bg-background">
-						{children}
-					</main>
+					<main className="px-8 pt-8 w-full text-text-primary h-full rounded-xl bg-background">{children}</main>
 				</QueryClientProvider>
 			</div>
 		</div>
