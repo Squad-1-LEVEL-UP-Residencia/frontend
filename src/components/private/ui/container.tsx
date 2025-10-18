f1e1f9add11574bd3490c2671d6d1b6e5e54a0c4
Danillo Ferreira <nilloferreiira@gmail.com>
2025-10-18 16:17:03 -0300

    fix: container and user-list scroll

--- src/components/private/ui/container.tsx
+++ src/components/private/ui/container.tsx
@@ -3,10 +3,13 @@ interface ContainerProps extends React.ComponentProps<"div"> {
 }
 
 export function Container({ className, variant, ...props }: ContainerProps) {
-	const containerStyle = variant === "page" ? "h-full rounded-b-none" : ""
+	const containerStyle = variant === "page" ? "h-[100dvh] rounded-b-none" : ""
 
 	return (
-		<div {...props} className={`w-full p-6 mx-auto bg-white rounded-lg shadow ${className} ${containerStyle}`}>
+		<div
+			{...props}
+			className={`w-full max-h-[70vh] p-6 mx-auto bg-white rounded-lg overflow-hidden shadow ${className} ${containerStyle}`}
+		>
 			{props.children}
 		</div>
 	)
--- src/components/private/usuarios/users-list.tsx
+++ src/components/private/usuarios/users-list.tsx
@@ -16,33 +16,35 @@ export function UsersList() {
 
 	return (
 		<Container variant="page">
-			<Table
-				isLoading={isLoading}
-				head={["Usuário", "E-mail", "Cargo", "Ações"]}
-				body={
-					users &&
-					users.map((element: User) => (
-						<Table.Row key={element.id} variant="row">
-							<Table.Data className="flex justify-start items-center gap-2">
-								<Avatar name={element.name} />
-								{element.name}
-							</Table.Data>
-							<Table.Data>{element.email}</Table.Data>
-							<Table.Data>{Array.isArray(element.role) ? element.role.join(", ") : "-"}</Table.Data>
-							<Table.Data className="flex justify-start items-center space-x-2">
-								<ModalTrigger id="edit_user_modal">
-									<Button outline={true}>Editar</Button>
-								</ModalTrigger>
-								<ModalTrigger id="delete_user_modal">
-									<Button outline={false} color="transparent" className="hover:text-red-500">
-										<Trash2Icon width={16} height={16} />
-									</Button>
-								</ModalTrigger>
-							</Table.Data>
-						</Table.Row>
-					))
-				}
-			/>
+			<div className="w-full max-h-[70vh] overflow-y-auto overflow-x-auto">
+				<Table
+					isLoading={isLoading}
+					head={["Usuário", "E-mail", "Cargo", "Ações"]}
+					body={
+						users &&
+						users.map((element: User) => (
+							<Table.Row key={element.id} variant="row">
+								<Table.Data className="flex justify-start items-center gap-2">
+									<Avatar name={element.name} />
+									{element.name}
+								</Table.Data>
+								<Table.Data>{element.email}</Table.Data>
+								<Table.Data>{Array.isArray(element.role) ? element.role.join(", ") : "-"}</Table.Data>
+								<Table.Data className="flex justify-start items-center space-x-2">
+									<ModalTrigger id="edit_user_modal">
+										<Button outline={true}>Editar</Button>
+									</ModalTrigger>
+									<ModalTrigger id="delete_user_modal">
+										<Button outline={false} color="transparent" className="hover:text-red-500">
+											<Trash2Icon width={16} height={16} />
+										</Button>
+									</ModalTrigger>
+								</Table.Data>
+							</Table.Row>
+						))
+					}
+				/>
+			</div>
 		</Container>
 	)
 }
