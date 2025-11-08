c9224aa204c1d97bdb0f3520162f5955126ffda3
Danillo Ferreira <nilloferreiira@gmail.com>
2025-11-08 14:18:38 -0300

    fix: sign-out from the server

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,11 +1,37 @@
+import { useToken } from "@/hooks/use-token"
+import { env } from "@/lib/env"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
 	const url = request.nextUrl.clone()
-	url.pathname = "/sign-in"
+	url.pathname = "/sign-in" // request pro servidor
 
+	const accessToken = await useToken()
+	if (!accessToken) {
+		return null
+	}
+
+	const api = env.NEXT_PUBLIC_API_URL
+	const result = await fetch(`${api}/auth/sign-out`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${accessToken}`
+		}
+	})
+
+	if (!result.ok) {
+		const errorText = await result.text()
+		console.log("Erro ao deslogar o usuário")
+		console.log(result)
+		return {
+			ok: false,
+			status: result.status,
+			error: errorText
+		}
+	}
 	const response = NextResponse.redirect(url)
-	response.cookies.delete("accessToken")
+	response.cookies.delete("access_token")
 
 	return response
 }
