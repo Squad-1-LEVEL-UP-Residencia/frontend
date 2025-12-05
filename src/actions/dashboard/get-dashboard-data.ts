b8adf29ebdbed151adac7f42772eab6b986d9287
Caio Vasconcelos <caioxb@hotmail.com>
2025-12-05 15:36:35 -0300

    Action Dashboard

---
+++ src/actions/dashboard/get-dashboard-data.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { env } from "@/lib/env"
+import { useToken } from "@/hooks/use-token"
+import { DashboardData } from "@/types/dashboard/dashboard"
+
+export async function getDashboardData(userId?: string) {
+	const accessToken = await useToken()
+	if (!accessToken) {
+		return {} as DashboardData
+	}
+
+	const api = env.NEXT_PUBLIC_API_URL
+
+	// Constrói a URL com a query string se userId for fornecido
+	const queryParams = userId ? `?user=${userId}` : ""
+	const url = `${api}/dashboard${queryParams}`
+
+	const result = await fetch(url, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${accessToken}`
+		}
+	})
+
+	if (!result.ok) {
+		console.error("Failed to fetch dashboard data:", result.statusText)
+		return {} as DashboardData
+	}
+
+	const response = await result.json()
+	return response as DashboardData
+}
---
+++ src/hooks/dashboard/use-dashboard.ts
@@ -0,0 +1,11 @@
+import { getDashboardData } from "@/actions/dashboard/get-dashboard-data"
+import { DashboardData } from "@/types/dashboard/dashboard"
+import { useQuery } from "@tanstack/react-query"
+
+export function useDashboard(userId?: string) {
+	return useQuery<DashboardData>({
+		queryKey: ["dashboard", userId ?? "all"],
+		queryFn: () => getDashboardData(userId),
+		staleTime: 1000 * 60 * 5 // 5 minutos
+	})
+}
